beac1967758b29e6a98b5abbf5f106d64e77f6d0
Gerardo <everythingissofast@gmail.com>
2024-05-25 13:16:35 -0600

    demostracion de casting en double e int

--- src/Main.java
+++ src/Main.java
@@ -18,13 +18,16 @@ public class Main {
                 """ + fechaDeLanzamiento;
         System.out.println(sinopsis);
 
-        //Ejemplo del metodo String.format
+        int clasificacion = (int) media/2;
+        System.out.println("Clasificacion: "+clasificacion);
+
+        //*****Ejemplo del metodo String.format
         String nombre = "Maria";
         int edad = 30;
         double valor = 5.9999;
         System.out.println(String.format("Mi nombre es %s, tengo %d y hoy gaste %.2f dolares",nombre,edad,valor));
 
-        //Ejemplo del metodo formatted usado dentro de un bloque de texto
+        //*****Ejemplo del metodo formatted usado dentro de un bloque de texto
         String nombre2 = "Gerardo";
         int aulas = 4;
 
@@ -37,13 +40,21 @@ public class Main {
                 """.formatted(nombre2,aulas);
         System.out.println(mensaje);
 
-        //castin implicito: se realiza automaticamente por el compulador cuando el tipo de dato de origen es compatible con el tipo de datos de destino.
+        //*****castin implicito: se realiza automaticamente por el compulador cuando el tipo de dato de origen es compatible con el tipo de datos de destino.
         //por ejemplo, es posible asignar un valor de tipo int a una variable de tipo double, ya que el tipo double es mas grande y puede almacenar todos los valores que el t
         //tipo int puede contener:
 
         int x = 10;
         double y = x;
         System.out.println("Valor de y: " +y);
+
+        /*
+        *****casting explicito: se realiza cuando el tipo de datos de origen es incompatible
+        con el tipo de dato de destino. En este caso, debemos utilizar el operador de casting para realizar la conversion
+         */
+        double m = 10.5;
+        int n = (int) m; //casting explicito
+        System.out.println("el valor de casting de n a m: "+n);
 
     }
 }
