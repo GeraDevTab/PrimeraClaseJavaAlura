d673f27a8d191480f839490c7aca7e0821a39b69
Gerardo <everythingissofast@gmail.com>
2024-05-25 16:09:35 -0600

    creacion de clase decisiones y lectura

---
+++ src/Decisiones.java
@@ -0,0 +1,23 @@
+public class Decisiones {
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDelaPelicula = 8.2;
+        String tipoPlan = "plus";
+
+        //codigo que pruyeba los operadores relacionales
+        if(fechaDeLanzamiento >= 2022){
+            System.out.println("Peliculas mas populares");
+        }
+        else {
+            System.out.println("Peliculas retro que aun vale la pena ver");
+        }
+
+        if(incluidoEnElPlan && tipoPlan.equals("plus")){
+            System.out.println("Disfrute de su pelicula");
+        }
+        else {
+            System.out.println("Pelicula no disponible en su plan actual");
+        }
+    }
+}
